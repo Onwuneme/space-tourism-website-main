d4dd41d3d2806f64b47e38156567a82712f617d7
Onwuneme <146716349+Onwuneme@users.noreply.github.com>
2025-07-20 00:21:20 +0100

    work done

--- src/Component/CustomHook/Toggle.jsx
+++ src/Component/CustomHook/Toggle.jsx
@@ -3,5 +3,6 @@ import { useState } from "react";
 export default function useToggle(initalValue =false){
     const[value,setValue]=useState(initalValue)
     const toggle = ()=>setValue(prev=>!prev)
-    return[value,toggle]
+    const close = ()=>setValue(false)
+    return[value,toggle,close]
 }
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Component/Layout/Navbar";
+import Navbar from "../Component/Navbar";
 
 export default function Layout(){
     return(
